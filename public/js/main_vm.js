7ee60a21a20bbb04eca92c816c9161f9d80c30cd
MacDonald <k_macdonald80262@ats-hr420-dm27.fc.ca>
2020-01-28 17:37:39 -0500

    added

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,11 +1,35 @@
 // imports always go first - if we're importing anything
-
+import ChatMessage from "./modules/ChatMessage.js";
 const socket = io();
 
 function setUserId(packet) {
     debugger;
     console.log(packet);
 }
+
+//this is our main Vue instance
+const vm = new VTTCue({
+    data: {
+        messages: [
+            {
+
+        message: {
+            name: "TVR",
+            content: "hey wassup"
+        }
+    }
+    ]
+    },
+
+    components: {
+        newmessage: ChatMessage
+    },
+
+    mounted: function() {
+        console.log('mounted');
+    
+    }
+}).$mount("#app");
 
 //some event handling -> these events are coming from the server
 socket.addEventListener('connected', setUserId);
